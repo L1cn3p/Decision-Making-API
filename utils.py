e5652a0197cb25f4b8bcfd8f6c713a0684e741fb
Fahad Almana <falmana@barzanholdings.com>
2022-05-19 10:42:17 +0300

    created problem class and problem factory

--- utils.py
+++ utils.py
@@ -1,10 +1,7 @@
 import json
 
-"""
-for each object in our asset_list we should create an Asset and give it a key (int: this integer refers to the index of the 
-individual asset in our algorithm response)
-"""
-assets = [
+
+ASSETS = [
     ["Fire Engine A", 80, ["Oil","Chemical","Electrical"], 5, ["Supress","Control"]],
     ["Fire Engine B", 90, ["Chemical","Electrical"], 3, ["Supress","Control"]],
     ["Fire Fast Response", 100, ["Electrical"], 2, ["Control"]],
@@ -25,6 +22,15 @@ class Asset:
         self.location = location
 
 
+"""
+To use this class initialize it and call create_asset_dict with a list of json objects as an argument
+see object.json for an example of what the inputted list in this method should look like.
+factory = AssetFactory()
+asset_dict = factory.create_asset_dict(request)
+returns a dictionary of the assets we want with an int as a key.
+This key will correspond to the index of its respective asset in the "individual" we use in the algorithm.
+"""
+
 class AssetFactory:
     def __init__(self):
         self.asset_list = []
@@ -32,53 +38,54 @@ class AssetFactory:
     def _MakeFireEngineA(self, location, quantity):
          
         if quantity > 0:
-            self.asset_list.append(Asset(*assets[0], location=location))
+            self.asset_list.append(Asset(*ASSETS[0], location=location))
             self._MakeFireEngineA(location=location, quantity=quantity-1)
         return 
 
     def _MakeFireEngineB(self, location, quantity):
         
         if quantity > 0:
-            self.asset_list.append(Asset(*assets[1], location=location))
+            self.asset_list.append(Asset(*ASSETS[1], location=location))
             self._MakeFireEngineB(location=location, quantity=quantity-1)
         return 
 
     def _MakeFireFastResponse(self, location, quantity):
         
         if quantity > 0:
-            self.asset_list.append(Asset(*assets[2], location=location))
+            self.asset_list.append(Asset(*ASSETS[2], location=location))
             self._MakeFireFastResponse(location=location, quantity=quantity-1)
         return 
 
     def _MakeAmbulance(self, location, quantity):
         
         if quantity > 0:
-            self.asset_list.append(Asset(*assets[3], location=location))
+            self.asset_list.append(Asset(*ASSETS[3], location=location))
             self._MakeAmbulance(location=location, quantity=quantity-1)
         return 
 
     def _MakeMedicalFastResponse(self, location, quantity):
         
         if quantity > 0:
-            self.asset_list.append(Asset(*assets[4], location=location))
+            self.asset_list.append(Asset(*ASSETS[4], location=location))
             self._MakeMedicalFastResponse(location=location, quantity=quantity-1)
         return 
 
     def _MakePolicePatrolCar(self, location, quantity):
         
         if quantity > 0:
-            self.asset_list.append(Asset(*assets[5], location=location))
+            self.asset_list.append(Asset(*ASSETS[5], location=location))
             self._MakePolicePatrolCar(location=location, quantity=quantity-1)
         return 
 
     def _MakeTrafficPoliceBike(self, location, quantity):
         
         if quantity > 0:
-            self.asset_list.append(Asset(*assets[6], location=location))
+            self.asset_list.append(Asset(*ASSETS[6], location=location))
             self._MakeTrafficPoliceBike(location=location, quantity=quantity-1)
         return 
     
-    def create_asset_list(self, assets_list):
+    def create_asset_dict(self, assets_list):
+        self.asset_dict = {}
         for asset in assets_list:
             _type = asset["type"]
             location = asset["location"]
@@ -100,16 +107,53 @@ class AssetFactory:
                 case "Traffic Police Bike":
                     self._MakeTrafficPoliceBike(location=location, quantity=quantity)
 
-        
+        for i, asset in enumerate(self.asset_list):
+            self.asset_dict[i] = asset
+        return self.asset_dict
 
-factory = AssetFactory()
 
-with open('object.json', 'r') as f:
-    data = json.load(f)
+# TODO create a situation builder,
+# the situation builder should return a list of problem objects that
+# the algorithm has to solve squentially.
 
-request = data["assets_list"]
-factory.create_asset_list(request)
+class Problem:
+    def __init__(self, name, severity, action, location, time_constraint):
+        self.name = name
+        self.severity = severity
+        self.action = action
+        self.location = location
+        self.time_constraint = time_constraint
 
-print(factory.asset_list)
-print(len(factory.asset_list))
+class ProblemFactory:
+
+    def create_problem_list(self,situation_list):
+        self.problem_list = []
+        for problem in situation_list:
+            name = problem["type"]
+            severity = problem["severity"]
+            action = problem["action"]
+            location = problem["location"]
+            time_constraint = problem["time_constraint"]
+
+            problem = Problem(name, severity, action, location, time_constraint)
+            self.problem_list.append(problem)
+
+        return self.problem_list
+    
+
+if __name__ == "__main__":
+    with open('object.json', 'r') as f:
+        data = json.load(f)
+    assets_list = data["assets_list"]
+    situation_list = data["situation_list"]
+
+    asset_factory = AssetFactory()
+    problem_factory = ProblemFactory()
+
+    asset_dict = asset_factory.create_asset_dict(assets_list)
+    problem_list = problem_factory.create_problem_list(situation_list)
+    # print(asset_dict.items())
+    # print(problem_list)
+    print(vars(asset_dict[0]))
+    print(vars(problem_list[0]))
 
