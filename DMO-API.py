00c56a8c3804bafc22097467001b2a8b35c3442b
Fahad Almana <falmana@barzanholdings.com>
2022-05-19 12:31:36 +0300

    created API that correctly receives post requests and parses the data

--- DMO-API.py
+++ DMO-API.py
@@ -1,3 +1,32 @@
 from utils import *
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+
+"""
+This API will reveive an json request (see object.json for example)
+and will send this request to be procossed by the alogrithm.
+It should respond with a json object of the appropriate response to each situation
+"""
+
+app = FastAPI()
+
+@app.post("/DMO")
+async def submit(request: Request):
+    # return {str(assets_list) : str(situation_list)}
+    data = await request.json()
+    
+    assets_list = data["assets_list"]
+    situation_list = data["situation_list"]
+
+    asset_factory = AssetFactory()
+    problem_factory = ProblemFactory()
+
+    asset_dict = asset_factory.create_asset_dict(assets_list)
+    problem_list = problem_factory.create_problem_list(situation_list)
+
+    response = str(asset_dict) + str(problem_list)
+
+    
+    return {"message": response}
+
 
